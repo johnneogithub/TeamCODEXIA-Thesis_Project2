7b752d7893af1635aba3056b3dc8ac6899675c5d
John Ray Gloria <147296265+johnraygloria@users.noreply.github.com>
2024-09-08 17:16:06 +0800

    Almost done on responsive

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -6,33 +6,31 @@ import Footer from '../Components/Global/Footer';
 
 function Home() {
   return (
-<>
-
+    <>
     <div className='background_home'>
       <Nav/>
-
-      <div className='picture_fix'>
-        <img className="home_picture" src={newhome} alt="" />
-      </div>
-
-    <div className="home_intro-style">
-        <p className="first_para-1">Let us plan your future, for a better tomorrow.</p>
-        <p className="second_para">PlanitFamIt is designed to reach every Filipinos that seeks better future for their family, through family planning.</p>
-        
-        <div className="flex_more">
-        <Link to="/Chatbot" className="assistant_intro" href="">Planlt Assistant</Link>
-        <Link to="/Articles" className="assistant_intro" href="">Healthcare Articles</Link>
+      <div className='content_wrapper'>
+        <div className='picture_fix'>
+          <img className="home_picture" src={newhome} alt="PlanitFamIt background" />
         </div>
+        <div className="home_intro-style">
+          <h1 className="main_title">Let us plan your future, for a better tomorrow.</h1>
+          <p className="sub_title">PlanitFamIt is designed to reach every Filipino that seeks a better future for their family, through family planning.</p>
+          
+          <div className="cta_container">
+            <Link to="/Chatbot" className="cta_button cta_button--primary">PlanIt Assistant</Link>
+            <Link to="/Articles" className="cta_button cta_button--secondary">Healthcare Articles</Link>
+          </div>
 
-        <div className="fix_moko">
-          <Link to="/Type" className='button_type'>Check your healthcare </Link>
-        </div>    
+          <div className="healthcare_check">
+            <Link to="/Type" className='cta_button cta_button--tertiary'>Check your healthcare</Link>
+          </div>    
+        </div>
       </div>
     </div>
-
     <Footer/>
-    </>
-    );
-    }
+  </>
+);
+}
 
 export default Home;
